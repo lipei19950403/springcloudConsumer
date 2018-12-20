78d0d4907924a0b97f61b83d9340130ff332a1c3
lipei19950403 <996891845@qq.com>
2018-12-20 17:22:43 +0800

    111

---
+++ src/main/java/com/csii/lipei/springcloudCustomer/controller/MovieController.java
@@ -0,0 +1,19 @@
+package com.csii.lipei.springcloudCustomer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+	
+	@Autowired
+	public RestTemplate template;
+	
+	@GetMapping("/customer/test")
+	public String send(){
+		return this.template.getForObject("http://localhost:8082/provide/test", String.class);
+	}
+	
+}
